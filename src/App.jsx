f5883ac4f64dd86410e9ba214a0a6c3d10e1855d
Laurenz Farthofer <laurenz@hey.com>
2024-04-05 11:42:44 +0000

    modified website content

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Laurenz Farthofer",
+  title: "PhD Student & RnD Software Developer",
+  email: "laurenz@hey.com",
+  gitHub: "https://github.com/laurenzbeck",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/laurenz-farthofer-50156b219/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
